51ff119795cd2eb84255ce6b8026197f92678f0d
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-29 22:37:54 +0300

    Question 1

---
+++ Assignment/Duration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    #region Question (1)
+    public class Duration
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public Duration()
+        {
+            Hours = 0;
+            Minutes = 0;
+            Seconds = 0;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Duration: {Hours}:{Minutes}:{Seconds}");
+        }
+        #endregion
+    }
+}
